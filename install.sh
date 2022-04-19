4551909e0ab3b78cd9c9f7f7c4e085ba266a5a5e
pyro57 <kevinpyrosc@gmail.com>
2022-04-18 19:54:44 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 cargo build
 sudo mv ./target/debug/flatman /usr/bin
+mkdir -p ~/.cache/flatman_install
+if (pacman -Q paru)
+then
+echo "paru already installed, skipping installation"
+else
+cd ~/.cache/flatman_install
+sudo pacman -S git
+git clone https://aur.archlinux.org/paru
+cd paru
+makepkg -si
+cd ~
+fi
